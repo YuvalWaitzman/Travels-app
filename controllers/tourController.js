7a75c97aae45c4107ca7e5cb61f9065e36bac03e
Yuval Waitzman <yuvalwaitzman888@gmail.com>
2023-02-06 15:00:12 +0200

    setting query object

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,7 +6,19 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //PREPARE FILTER FEATURE - ignore page,sort, limit and fields parameters.
+
+    //BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => {
+      delete queryObj[el];
+    });
+    //EXECUTE QUERY
+    const query = Tour.find(queryObj);
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
