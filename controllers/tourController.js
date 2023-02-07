3539f632444812ea6c6108191bc0fe93e81d8591
Yuval Waitzman <yuvalwaitzman888@gmail.com>
2023-02-07 16:17:01 +0200

    alias feature enabled

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,15 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  console.log(req.query);
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    //PREPARE FILTER FEATURE - ignore page,sort, limit and fields parameters.
-
     //BUILD QUERY
     //1A) Filtering
     const queryObj = { ...req.query };
