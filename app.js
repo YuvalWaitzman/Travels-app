a5c8a1f1be58f2b0b5056f25e3830b888d2d56bd
Yuval Waitzman <yuvalwaitzman888@gmail.com>
2023-02-12 18:27:44 +0200

    fixing "year" problem in stats handler

--- app.js
+++ app.js
@@ -24,4 +24,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+//Handling non existing routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
