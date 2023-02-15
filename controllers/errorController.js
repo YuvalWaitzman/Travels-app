315b3850e56e6e5e9a7fcd1a1f11239527048956
Yuval Waitzman <yuvalwaitzman888@gmail.com>
2023-02-15 17:13:59 +0200

    add a comment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -46,8 +46,10 @@ const sendErrorProd = (err, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+  //developement env
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
+    // production env
   } else if (process.env.NODE_ENV === 'production') {
     let error;
     if (err.name === 'CastError') error = handleCastErrorDB(err);
