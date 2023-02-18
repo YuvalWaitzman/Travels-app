a3866ace5d9c330029fc5449c6be380d207180aa
Yuval Waitzman <yuvalwaitzman888@gmail.com>
2023-02-18 02:05:29 +0200

    implementing login and signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,44 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+const AppError = require('./../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  //check if email and password exist
+
+  if (!email || !password) {
+    const error = new AppError('Please provide email and password', 400);
+    return next(error);
+  }
+
+  //check if the user exist && password is correct
+  const user = User.findOne({ email });
+
+  // send the token to the client
+  token = '';
+  res.status(200).json({
+    stauts: 'success',
+    token,
+  });
+};
