c7a964b8cc69505e34f19db2ebd10005d3c52f21
Yuval Waitzman <yuvalwaitzman888@gmail.com>
2023-02-18 02:05:46 +0200

    implementing signup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,8 @@ const userController = require('./../controllers/userController');
 const authController = require('../controllers/authController');
 const router = express.Router();
 
+router.post('/login', authController.login);
+
 router.post('/signup', authController.signup);
 
 router
